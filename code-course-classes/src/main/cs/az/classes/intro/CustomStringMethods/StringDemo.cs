0085a34713e53c6ef6946e2140783c6ffbfdfcb3
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-10-17 02:27:41 +0400

    enhance string manipulation capabilities: custom implementations for LastIndexOf, Contains, Replace, Substring, and Trim methods

---
+++ code-course-classes/src/main/cs/az/classes/intro/CustomStringMethods/StringDemo.cs
@@ -0,0 +1,26 @@
+﻿namespace code_course_classes.main.cs.az.classes.intro.CustomStringMethods;
+
+public class StringDemo
+{
+    
+    public static void Main(string[] args)
+    {
+        StringMethods stringMethods = new StringMethods();
+        string exp = "   Hello, World!   ";
+
+        Console.WriteLine(stringMethods.CustomLastIndexOf(exp, 'W'));
+
+        bool contains = stringMethods.CustomContains(exp, 'h');
+        Console.WriteLine(contains);
+
+        string replaced = stringMethods.CustomReplace(exp, "World", "Dunya");
+        Console.WriteLine(replaced);
+
+        string substring = stringMethods.CustomSubstring(exp, 2, 5);
+        Console.WriteLine(substring);
+
+        string trim = stringMethods.CustomTrim(exp);
+        Console.WriteLine(trim);
+    }
+    
+}
---
+++ code-course-classes/src/main/cs/az/classes/intro/CustomStringMethods/StringMethods.cs
@@ -0,0 +1,65 @@
+﻿namespace code_course_classes.main.cs.az.classes.intro.CustomStringMethods;
+
+public class StringMethods
+{
+    
+    public int CustomLastIndexOf(string source, char value)
+    {
+        {
+            if (string.IsNullOrEmpty(source))
+            {
+                return -1;
+            }
+
+            int length = source.Length;
+            for (int i = length - 1; i >= 0; i--)
+            {
+                if (i < length && source[i] == value)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+
+    public bool CustomContains(string source, char value)
+    {
+        if (source == null)
+        {
+            throw new ArgumentNullException(nameof(source));
+        }
+
+        foreach (char c in source)
+        {
+            if (c == value)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public string CustomReplace(string source, string oldValue, string newValue)
+    {
+        return source.Replace(oldValue, newValue);
+    }
+
+    public string CustomSubstring(string source, int startIndex, int length)
+    {
+        if (startIndex < 0 || startIndex + length > source.Length)
+        {
+            Console.WriteLine("Invalid startIndex or length");
+        }
+
+        return source.Substring(startIndex, length);
+    }
+
+    public string CustomTrim(string source)
+    {
+        return source.Trim();
+    }
+    
+}
