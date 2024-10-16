8e50e634e11395b42eca33769eacffa14edd2f15
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-10-17 03:02:15 +0400

    add Person class with FullName, Age, and PhoneNumber validation

---
+++ code-course-classes/src/main/cs/az/classes/intro/UserProfiles/Person.cs
@@ -0,0 +1,63 @@
+﻿namespace code_course_classes.main.cs.az.classes.intro.UserProfiles;
+
+public class Person
+{
+    string _fullName;
+    int _age;
+    string _phoneNumber;
+
+    public string FullName
+    {
+        get => _fullName;
+        set
+        {
+            if (IsValidFullName(value))
+            {
+                _fullName = value;
+            }
+            else
+            {
+                Console.WriteLine("Full name must contain at least two words, both starting with uppercase letters.");
+            }
+        }
+    }
+
+    public int Age
+    {
+        get => _age;
+        set
+        {
+            if (_age < 0)
+            {
+                Console.WriteLine("Age must be greater than zero.");
+            }
+
+            _age = value;
+        }
+    }
+
+    public string PhoneNumber
+    {
+        get => _phoneNumber;
+        set
+        {
+            if (!value.StartsWith("+994"))
+            {
+                _phoneNumber = "+994" + value.TrimStart('0');
+            }
+            else
+            {
+                _phoneNumber = value;
+            }
+        }
+    }
+
+    public bool IsValidFullName(string fullName)
+    {
+        var parts = fullName.Split(' ');
+        return parts.Length >= 2 &&
+               char.IsUpper(fullName[0]) &&
+               char.IsUpper(fullName[fullName.IndexOf(' ') + 1]);
+    }
+    
+}
---
+++ code-course-classes/src/main/cs/az/classes/intro/UserProfiles/UserProfileApp.cs
@@ -0,0 +1,22 @@
+﻿namespace code_course_classes.main.cs.az.classes.intro.UserProfiles;
+
+public class UserProfileApp
+{
+    
+    public static void Main(string[] args)
+    {
+        Person person1 = new Person();
+        person1.FullName = "John Doe";
+        person1.Age = 25;
+        person1.PhoneNumber = "501234567";
+
+        Person person2 = new Person();
+        person2.FullName = "jane doe";
+        person2.Age = -5;             
+        person2.PhoneNumber = "+994551234567";
+
+        Console.WriteLine($"Ad: {person1.FullName}, Yaş: {person1.Age}, Telefon: {person1.PhoneNumber}");
+        Console.WriteLine($"Ad: {person2.FullName}, Yaş: {person2.Age}, Telefon: {person2.PhoneNumber}");
+    }
+    
+}
