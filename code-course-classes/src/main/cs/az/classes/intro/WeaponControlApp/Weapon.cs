d77b7e171a263cc0b2e7626449ce7df6769c7b40
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-10-18 12:20:28 +0400

    add weapon control app: manage bullet capacity, shooting modes, and reloading

---
+++ code-course-classes/src/main/cs/az/classes/intro/WeaponControlApp/Weapon.cs
@@ -0,0 +1,108 @@
+﻿namespace code_course_classes.main.cs.az.classes.intro.WeaponControlApp;
+
+public class Weapon
+{
+    private int bullet_Capacity;
+    private int current_Bullets;
+    public bool isAutomatic;
+
+    public int Bullet_Capacity
+    {
+        get => bullet_Capacity;
+        set
+        {
+            if (value < 0)
+            {
+                Console.WriteLine("Bullet capacity cannot be negative.");
+            }
+            else
+            {
+                bullet_Capacity = value;
+
+                if (current_Bullets > bullet_Capacity)
+                {
+                    current_Bullets = bullet_Capacity;
+                }
+            }
+        }
+    }
+
+    public int Current_Bullets
+    {
+        get => current_Bullets;
+        set
+        {
+            if (value < 0)
+            {
+                Console.WriteLine("Bullet count cannot be negative.");
+            }
+            else if (value > bullet_Capacity)
+            {
+                Console.WriteLine(
+                    $"Bullet count cannot exceed capacity ({bullet_Capacity}). Please enter a valid bullet count.");
+                while (value > bullet_Capacity)
+                {
+                    Console.WriteLine($"The magazine capacity is {bullet_Capacity}, please enter a valid count:");
+                    value = int.Parse(Console.ReadLine());
+                }
+
+                current_Bullets = value;
+            }
+            else
+            {
+                current_Bullets = value;
+            }
+        }
+    }
+
+    public bool IsAutomatic
+    {
+        get => isAutomatic;
+        set => isAutomatic = value;
+    }
+
+    public Weapon(int bulletCapacity, int currentBullets, bool isAutomatic)
+    {
+        bullet_Capacity = bulletCapacity;
+        current_Bullets = currentBullets;
+        this.isAutomatic = isAutomatic;
+    }
+
+    public void Shoot()
+    {
+        if (current_Bullets == 0)
+        {
+            Console.WriteLine("No bullets in the magazine!");
+        }
+        else if (isAutomatic)
+        {
+            Console.WriteLine($"Automatic fire: {current_Bullets} bullets were fired.");
+            current_Bullets = 0;
+        }
+        else
+        {
+            current_Bullets--;
+            Console.WriteLine($"Single fire: 1 bullet fired, remaining bullets: {current_Bullets}");
+        }
+    }
+
+    public int GetRemainBulletCount()
+    {
+        return bullet_Capacity - current_Bullets;
+    }
+
+    public void Reload()
+    {
+        int neededBullets = GetRemainBulletCount();
+        current_Bullets = bullet_Capacity;
+        Console.WriteLine($"Magazine reloaded. {neededBullets} bullets added.");
+    }
+
+    public void ChangeFireMode()
+    {
+        isAutomatic = !isAutomatic;
+        string mode = isAutomatic ? "Automatic" : "Single fire";
+        Console.WriteLine($"Fire mode changed to: {mode}");
+    }
+    
+}
---
+++ code-course-classes/src/main/cs/az/classes/intro/WeaponControlApp/WeaponApp.cs
@@ -0,0 +1,88 @@
+﻿namespace code_course_classes.main.cs.az.classes.intro.WeaponControlApp;
+
+public class WeaponApp
+{
+    
+    public static void Main(string[] args)
+    {
+        Console.Write("Magazine capacity: ");
+        int bulletCapacity = int.Parse(Console.ReadLine());
+        Console.Write("Bullets in the magazine: ");
+        int currentBullets = int.Parse(Console.ReadLine());
+        Console.Write("Fire mode (enter '0' for single fire, '1' for automatic): ");
+        bool isAutomatic = Console.ReadLine() == "1";
+
+        Weapon weapon = new Weapon(bulletCapacity, currentBullets, isAutomatic);
+        while (true)
+        {
+            Console.WriteLine("\nSelect the desired command:");
+            Console.WriteLine("0 - Get information");
+            Console.WriteLine("1 - For Shoot method");
+            Console.WriteLine("2 - For GetRemainBulletCount method");
+            Console.WriteLine("3 - For Reload method");
+            Console.WriteLine("4 - For ChangeFireMode method");
+            Console.WriteLine("5 - To exit the program");
+            Console.WriteLine("6 - Modify settings");
+
+            int command = int.Parse(Console.ReadLine());
+
+            switch (command)
+            {
+                case 0:
+                    Console.WriteLine(
+                        $"Magazine capacity: {weapon.Bullet_Capacity}, Current bullet count: {weapon.Current_Bullets}, Fire mode: {(weapon.isAutomatic ? "Automatic" : "Single fire")}");
+                    break;
+                case 1:
+                    weapon.Shoot();
+                    break;
+                case 2:
+                    Console.WriteLine($"Bullets needed to fill the magazine: {weapon.GetRemainBulletCount()}");
+                    break;
+                case 3:
+                    weapon.Reload();
+                    break;
+                case 4:
+                    weapon.ChangeFireMode();
+                    break;
+                case 5:
+                    Console.WriteLine("Exiting the program...");
+                    return;
+                case 6:
+                    Console.WriteLine("Modify magazine capacity or bullet count:");
+                    Console.WriteLine("7 - Change magazine capacity");
+                    Console.WriteLine("8 - Change current bullet count");
+                    Console.WriteLine("9 - Go back to main menu");
+
+                    int subCommand = int.Parse(Console.ReadLine());
+
+                    switch (subCommand)
+                    {
+                        case 7:
+                            Console.WriteLine("Enter the new magazine capacity:");
+                            int newCapacity = int.Parse(Console.ReadLine());
+                            weapon.Bullet_Capacity = newCapacity;
+                            Console.WriteLine($"New magazine capacity: {weapon.Bullet_Capacity}");
+                            break;
+                        case 8:
+                            Console.WriteLine("Enter the new bullet count:");
+                            int newBullets = int.Parse(Console.ReadLine());
+                            weapon.Current_Bullets = newBullets;
+                            Console.WriteLine($"New bullet count: {weapon.Current_Bullets}");
+                            break;
+                        case 9:
+                            Console.WriteLine("Returning to main menu.");
+                            break;
+                        default:
+                            Console.WriteLine("Invalid selection. Please try again.");
+                            break;
+                    }
+
+                    break;
+                default:
+                    Console.WriteLine("Invalid selection. Please try again.");
+                    break;
+            }
+        }
+    }
+    
+}
