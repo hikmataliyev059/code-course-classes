105b85d69ac2df78e45d78e67fc4cb279ac04721
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-10-22 03:38:11 +0400

    implement Student class with basic info and methods

---
+++ code-course-classes/src/main/cs/az/classes/intro/StudentGroupManagement/Student.cs
@@ -0,0 +1,36 @@
+﻿namespace code_course_classes.main.cs.az.classes.intro.StudentGroupManagement;
+
+public class Student
+{
+    private static int _idCounter = 0;
+    private readonly int _id;
+    private string _fullName;
+    private double _point;
+
+    public Student(string fullName, double point)
+    {
+        _id = ++_idCounter;
+        _fullName = fullName;
+        _point = point;
+    }
+
+    public int Id => _id;
+
+    public string FullName
+    {
+        get => _fullName;
+        set => _fullName = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public double Point
+    {
+        get => _point;
+        set => _point = value;
+    }
+
+    public void StudentInfo()
+    {
+        Console.WriteLine($"Student info: fullName: {_fullName},point: {_point}, id: {_id}");
+    }
+    
+}
