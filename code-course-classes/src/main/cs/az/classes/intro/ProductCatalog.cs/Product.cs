b7ad536f912a1dee1a8dd2e1d9953840df867d07
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-10-25 00:35:35 +0400

    add product class with validation and type categorization

---
+++ code-course-classes/src/main/cs/az/classes/intro/ProductCatalog.cs/Product.cs
@@ -0,0 +1,45 @@
+﻿namespace code_course_classes.main.cs.az.classes.intro.ProductCatalog.cs;
+
+public class Product
+{
+    private string _name;
+    private int no;
+    private static int _noCounter = 0;
+    private double _price;
+    public Type type;
+
+    public Product(string name, double price, Type type)
+    {
+        _name = name;
+        no = ++_noCounter;
+        _price = price;
+        this.type = type;
+    }
+
+    public string Name
+    {
+        get => _name;
+        set => _name = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public int No1
+    {
+        get => no;
+        set => no = value;
+    }
+
+    public double Price
+    {
+        get => _price;
+        set
+        {
+            if (value < 0)
+            {
+                throw new PriceMustBeGreatherThanZeroException("Price cannot be negative");
+            }
+
+            _price = value;
+        }
+    }
+    
+}
