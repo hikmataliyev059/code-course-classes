8730a657c1ca46ec7a776c5e64427c9a0884d140
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-10-25 00:37:07 +0400

    add custom exception for handling price validation errors

---
+++ code-course-classes/src/main/cs/az/classes/intro/ProductCatalog.cs/PriceMustBeGreatherThanZeroException.cs
@@ -0,0 +1,8 @@
+﻿namespace code_course_classes.main.cs.az.classes.intro.ProductCatalog.cs;
+
+public class PriceMustBeGreatherThanZeroException : Exception
+{
+    public PriceMustBeGreatherThanZeroException(string? message) : base(message)
+    {
+    }
+}
