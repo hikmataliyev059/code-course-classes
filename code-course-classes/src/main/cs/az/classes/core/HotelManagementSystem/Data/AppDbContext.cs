c6ed365db22c84b83ae45c3d77683024e4f1e9b8
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-10-29 02:34:37 +0400

    add AppDbContext with methods for room management and reservation

---
+++ code-course-classes/src/main/cs/az/classes/core/HotelManagementSystem/Data/AppDbContext.cs
@@ -0,0 +1,49 @@
+﻿using code_course_classes.main.cs.az.classes.core.HotelManagementSystem.Helpers.Exceptions;
+using code_course_classes.main.cs.az.classes.core.HotelManagementSystem.Models;
+
+namespace code_course_classes.main.cs.az.classes.core.HotelManagementSystem.Data;
+
+public static class AppDbContext
+{
+    
+    private static List<Room> Rooms = new List<Room>();
+    
+    public static void AddRoom(Room room)
+    {
+        Rooms.Add(room);
+    }
+
+    public static List<Room> FindAllRoom(double price)
+    {
+        return Rooms.FindAll(x => x._price == price);
+    }
+
+    public static void MakeReservation(int? roomId, int userId)
+    {
+        if (roomId == null)
+        {
+            throw new NullReferenceException("Room ID cannot be null.");
+        }
+
+        Room room = Rooms.Find(x => x.Id == roomId);
+
+
+        if (room == null)
+        {
+            throw new NullReferenceException("This room has not been available.");
+        }
+
+        if (!room.IsAvailable)
+        {
+            throw new NotAvailableException("This room has already been reserved.");
+        }
+
+        if (userId > room._personCapacity)
+        {
+            throw new ArgumentException("The number of users exceeds the room capacity.");
+        }
+
+        room.IsAvailable = false;
+    }
+    
+}
