555cc221651e9f8b1fd1f1d11b2cef8813fc1871
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-10-29 02:30:18 +0400

    create room class with basic attributes and properties

---
+++ code-course-classes/src/main/cs/az/classes/core/HotelManagementSystem/Models/Room.cs
@@ -0,0 +1,31 @@
+﻿namespace code_course_classes.main.cs.az.classes.core.HotelManagementSystem.Models;
+
+public class Room
+{
+    
+    private static int _noCounter;
+    public int Id { get; }
+    private string _name;
+    public double _price;
+    public int _personCapacity;
+    public bool IsAvailable { get; set; } = true;
+
+    public Room(string name, double price, int personCapacity)
+    {
+        Id = ++_noCounter;
+        _name = name;
+        _price = price;
+        _personCapacity = personCapacity;
+    }
+
+    public string ShowInfo()
+    {
+        return $"Name: {_name}, Price: {_price}, Capacity: {_personCapacity}";
+    }
+
+    public override string ToString()
+    {
+        return ShowInfo();
+    }
+    
+}
