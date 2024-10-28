e5e2589822f5ad995606a5cab6f63d2786799b0f
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-10-29 02:33:35 +0400

    create custom NotAvailableException for unavailable room errors

---
+++ code-course-classes/src/main/cs/az/classes/core/HotelManagementSystem/Helpers/Exceptions/NotAvailableException.cs
@@ -0,0 +1,8 @@
+﻿namespace code_course_classes.main.cs.az.classes.core.HotelManagementSystem.Helpers.Exceptions;
+
+public class NotAvailableException : Exception
+{
+    public NotAvailableException(string? message) : base(message)
+    {
+    }
+}
